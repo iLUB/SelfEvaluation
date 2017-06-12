8b2336e1a61bfe24832e867ecd8cdd1ed3183f95
Timon Amstutz <timon.amstutz@bluewin.ch>
2017-06-12 13:47:20 +0200

    Added middle line in charts

--- classes/Feedback/class.ilLeftRightChart.php
+++ classes/Feedback/class.ilLeftRightChart.php
@@ -47,12 +47,27 @@ class ilLeftRightChart extends ilChartGrid
 	public function parseGlobalOptions(stdClass $a_options)
 	{
 		parent::parseGlobalOptions($a_options);
+		$x_tick_key = array_keys($this->getTicks()["x"]);
+		$y_tick_key = array_keys($this->getTicks()["y"]);
+
+
 		$a_options->{"yaxis"}->labelWidth = 0;
 
-		$a_options->{"yaxis"}->min = min(array_keys($this->getTicks()["y"]));
-		$a_options->{"yaxis"}->max = max(array_keys($this->getTicks()["y"]));
+		$a_options->{"yaxis"}->min = min($y_tick_key);
+		$a_options->{"yaxis"}->max = max($y_tick_key);
 
-		$a_options->{"xaxis"}->min = min(array_keys($this->getTicks()["x"]));
-		$a_options->{"xaxis"}->max = max(array_keys($this->getTicks()["x"]));
+		$a_options->{"xaxis"}->min = min($x_tick_key);
+		$a_options->{"xaxis"}->max = max($x_tick_key);
+
+		$a_options->{"grid"} = new stdClass();
+		$a_options->{"grid"}->{"markings"} = [];
+		$a_options->{"grid"}->{"markings"}[0] = new stdClass();
+		$a_options->{"grid"}->{"markings"}[0]->{"xaxis"} = new stdClass();
+		$middle = (min($x_tick_key)+max($x_tick_key))/2;
+		$a_options->{"grid"}->{"markings"}[0]->{"xaxis"}->from = $middle;
+		$a_options->{"grid"}->{"markings"}[0]->{"xaxis"}->to = $middle;
+		$a_options->{"grid"}->{"markings"}[0]->{"color"} = new stdClass();
+		$a_options->{"grid"}->{"markings"}[0]->{"color"} = "#333333";
+
 	}
 }
