e18c1878ba38df184292bd22264942a6afde35f6
Thomas Kolonko <thomas.kolonko@ilub.unibe.ch>
2017-10-10 15:53:48 +0200

    ilias53 ready

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'xsev';
 $version = '2.2.2';
 $ilias_min_version = '4.3.0';
-$ilias_max_version = '5.2.999';
+$ilias_max_version = '5.3.999';
 
 $responsible = "Timon Amstutz - University of Bern";
 $responsible_mail = "timon.amstutz@ilub.unibe.ch";
