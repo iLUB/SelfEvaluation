37ec02005e340a55b6ee693683ac4b78456da6f0
Timon Amstutz <timon.amstutz@bluewin.ch>
2019-03-10 21:20:17 +0100

    Print Update

---
+++ templates/js/print.js
@@ -0,0 +1,23 @@
+printFeedback = function() {
+    this.originalBodyWidht = $("body").width();
+
+    window.onafterprint = function(e){
+        $(window).off('mousemove', window.onafterprint);
+        $("body").width(this.originalBodyWidht);
+        console.log("On After Print");
+    };
+
+    $.when($("body").width(800)).then(
+        function () {
+            setTimeout(
+                function () {
+                    window.print();
+                    setTimeout(function(){
+                        $(window).one('mousemove', window.onafterprint);
+                        console.log("On After One");
+
+                    }, 1)
+                }, 500)
+        }
+    );
+}
