97bfe0fca72596e20cad0b871454eb2a1c822569
Timon Amstutz <timon.amstutz@bluewin.ch>
2020-04-06 16:17:25 +0200

    BIG BANG: Reformat code to new style guide II

--- templates/js/bar_spider_chart_toggle.js
+++ templates/js/bar_spider_chart_toggle.js
@@ -1,81 +1,83 @@
-xsevChartToggle = function(parent_id) {
-	this.parent = $('#' + parent_id);
+xsevChartToggle = function (parent_id) {
+    this.parent = $('#' + parent_id);
 
-	if(this.parent.find('button').length == 1){
-		this.parent.find('.btn-group').hide();
-		return;
-	}
+    if (this.parent.find('button').length == 1) {
+        this.parent.find('.btn-group').hide();
+        return;
+    }
 
-	this.bar_chart_button = this.parent.find('.bar_chart_button');
-	this.spider_chart_button = this.parent.find('.spider_chart_button');
-	this.left_right_chart_button = this.parent.find('.left_right_chart_button');
+    this.bar_chart_button = this.parent.find('.bar_chart_button');
+    this.spider_chart_button = this.parent.find('.spider_chart_button');
+    this.left_right_chart_button = this.parent.find('.left_right_chart_button');
 
-	this.bar_chart = this.parent.find('.bar_chart');
-	this.spider_chart = this.parent.find('.spider_chart');
-	this.left_right_chart = this.parent.find('.left_right_chart');
-	var self = this;
+    this.bar_chart = this.parent.find('.bar_chart');
+    this.spider_chart = this.parent.find('.spider_chart');
+    this.left_right_chart = this.parent.find('.left_right_chart');
+    var self = this;
 
-	this.first_button = null;
+    this.first_button = null;
 
-	if(this.bar_chart_button.length){
-		this.first_button = this.bar_chart_button;
-	}else if(this.spider_chart_button.length){
-		this.first_button = this.spider_chart_button;
-	}else{
-		this.first_button = this.left_right_chart_button;
-	}
+    if (this.bar_chart_button.length) {
+        this.first_button = this.bar_chart_button;
+    } else if (this.spider_chart_button.length) {
+        this.first_button = this.spider_chart_button;
+    } else {
+        this.first_button = this.left_right_chart_button;
+    }
 
-	this.hideIfLoaded = function(depth) {
-		if((self.spider_chart.find("canvas").length ||
-				self.left_right_chart.find("canvas").length)
-				|| depth > 100){
+    this.hideIfLoaded = function (depth) {
+        if ((self.spider_chart.find("canvas").length ||
+            self.left_right_chart.find("canvas").length)
+            || depth > 100) {
 
-			if(self.spider_chart_button != self.first_button){
-				self.spider_chart.hide();
-			}
-			console.log();
-			self.left_right_chart.hide();
-		}else{
-			setTimeout(function() {self.hideIfLoaded(++depth)}, 100);
-		}
+            if (self.spider_chart_button != self.first_button) {
+                self.spider_chart.hide();
+            }
+            console.log();
+            self.left_right_chart.hide();
+        } else {
+            setTimeout(function () {
+                self.hideIfLoaded(++depth)
+            }, 100);
+        }
 
         return self;
-	};
+    };
 
-	this.first_button.addClass("active");
-	this.hideIfLoaded(0);
+    this.first_button.addClass("active");
+    this.hideIfLoaded(0);
 
-	this.deactivateButtons = function(){
-		self.bar_chart_button.removeClass("active");
-		self.spider_chart_button.removeClass("active");
-		self.left_right_chart_button.removeClass("active");
+    this.deactivateButtons = function () {
+        self.bar_chart_button.removeClass("active");
+        self.spider_chart_button.removeClass("active");
+        self.left_right_chart_button.removeClass("active");
 
-		self.bar_chart.hide();
-		self.spider_chart.hide();
-		self.left_right_chart.hide();
-	};
+        self.bar_chart.hide();
+        self.spider_chart.hide();
+        self.left_right_chart.hide();
+    };
 
-	this.bar_chart_button.click(function() {
-		self.deactivateButtons(self);
-		self.bar_chart_button.addClass("active");
-		self.bar_chart.show();
+    this.bar_chart_button.click(function () {
+        self.deactivateButtons(self);
+        self.bar_chart_button.addClass("active");
+        self.bar_chart.show();
         self.printFeedback();
 
         return false;
-	});
+    });
 
-	this.spider_chart_button.click(function() {
-		self.deactivateButtons(self);
-		self.spider_chart_button.addClass("active");
-		self.spider_chart.show();
-		return false;
-	});
+    this.spider_chart_button.click(function () {
+        self.deactivateButtons(self);
+        self.spider_chart_button.addClass("active");
+        self.spider_chart.show();
+        return false;
+    });
 
-	this.left_right_chart_button.click(function() {
-		self.deactivateButtons(self);
-		self.left_right_chart_button.addClass("active");
-		self.left_right_chart.show();
-		return false;
-	});
+    this.left_right_chart_button.click(function () {
+        self.deactivateButtons(self);
+        self.left_right_chart_button.addClass("active");
+        self.left_right_chart.show();
+        return false;
+    });
 };
 
--- templates/js/scale_units.js
+++ templates/js/scale_units.js
@@ -1,46 +1,47 @@
-function scaleUnits(){
+function scaleUnits() {
     this.full_unit_titels = new Array();
     /** Fix bootstrap naming collision with tooltip in ILIAS 4.3 **/
-    il.Tooltip.add = function (el_id, cfg) {};
+    il.Tooltip.add = function (el_id, cfg) {
+    };
     $("#help_tt").remove();
     /** End Fix **/
 
-    this.updateScaleUnits = function() {
+    this.updateScaleUnits = function () {
         var self = this;
-        $(".scale-units td div").each(function() {
+        $(".scale-units td div").each(function () {
             var id = $(this).attr("id");
             $(this).text(self.full_unit_titels[id]);
             if ($(this).prop('scrollWidth') > ($(this).width() + 1)) {
                 $(this).attr("data-toggle", "tooltip");
                 $(this).text(function () {
-                    return $(this).text().substring(0,3)+"...";
+                    return $(this).text().substring(0, 3) + "...";
                 });
                 $(this).tooltip('enable');
-                $(this).tooltip({trigger:'hover'});
+                $(this).tooltip({trigger: 'hover'});
             }
-            else{
+            else {
                 $(this).tooltip('disable');
                 $(this).text(self.full_unit_titels[id]);
             }
         });
     }
 
-    this.scaleMatrix = function() {
-        $(".matrix-row-input").each(function() {
+    this.scaleMatrix = function () {
+        $(".matrix-row-input").each(function () {
             parent_object = $(this).parents(".block-question");
             $(this).height(parent_object.children(".question-text").outerHeight());
         });
     }
 
-    $( window ).resize(function() {
+    $(window).resize(function () {
         self.updateScaleUnits();
         self.scaleMatrix();
 
     });
 
-    $( window ).load(function() {
+    $(window).load(function () {
         var self = this;
-        $(".scale-units td div").each(function() {
+        $(".scale-units td div").each(function () {
             var id = $(this).attr("id");
             self.full_unit_titels[id] = $(this).text();
         });
