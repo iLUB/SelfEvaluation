afd1a994f9fc760e6cff1bb498d808246c7eee2d
Olivia Kaufmann <olivia.kaufmann@ilub.unibe.ch>
2021-04-07 16:04:59 +0200

    Update for ilias7

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'xsev';
 $version = '3.2.0';
 $ilias_min_version = '5.3.0';
-$ilias_max_version = '6.999.999';
+$ilias_max_version = '7.999.999';
 $supports_export = true;
 
 $responsible = "Timon Amstutz - University of Bern";
